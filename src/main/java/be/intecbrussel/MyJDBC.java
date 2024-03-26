6b15f4fb1146b01514ef936d71e7cef0f4d45817
Mausam <mausamtiwari93@gmail.com>
2024-03-26 11:34:27 +0100

    First commit

---
+++ src/main/java/be/intecbrussel/MyJDBC.java
@@ -0,0 +1,23 @@
+package be.intecbrussel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MyJDBC {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdb", "chef", "P@ssw0rd");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from people");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstname"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/be/intecbrussel/sql/people.sql
@@ -0,0 +1,11 @@
+CREATE TABLE people
+(
+    id        INT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO people
+VALUES (0, 'Tom'),
+       (1, 'Mike');
+
